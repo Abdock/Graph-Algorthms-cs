4ba13a4b2c29e4c3c70cd849578998f8a0e7c71d
Abdusattar <abdusattar.kasymbekov@gmail.com>
2021-06-15 22:19:56 +0600

    Added new files
    Added new file Vertex.cs to work with graph

---
+++ Graph/GraphTheory/Vertex.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace GraphTheory
+{
+    public class Vertex//Class for describe the vertex of graph, will contain information about vertex, weight, degrees, neighbours for current vertex etc
+    {
+        public Dictionary<Vertex, int> Neighbours { get; set; }//first argument contain the neighbour and second contain weight of edge to neighbour
+        public int Degree { get; set; }
+    }
+}
